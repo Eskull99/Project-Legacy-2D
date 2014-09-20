037afa0c1228913d7d61f71b5a53deed91d87889
Patrick Conner <PConner000@gmail.com>
2014-09-19 23:02:07 -0700

    initiating repo with unity defaults and a basic sprite motor

---
+++ Project Legacy 2D/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMotor : MonoBehaviour {
+
+	public float maxSpeed = 5, maxSprintFactor = 2, maxSprintTime = .5f;
+	public Vector3 moveVector;
+	public bool canSprint = true;
+	private float curSprintFactor, curSprintTime;
+
+	// Use this for initialization
+	void Start () {
+		curSprintFactor = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		trackSprint ();
+		vectorMove ();
+	}
+
+	private void trackSprint(){
+		if (Input.GetAxis ("Sprint") > 0) {
+			if(canSprint == true){
+				curSprintFactor = maxSprintFactor * Input.GetAxis ("Sprint");
+				if (curSprintFactor < 1) {
+					curSprintFactor = 1;
+				}
+			}else{
+				curSprintFactor = 1;
+			}
+			curSprintTime -= Time.deltaTime;
+			if(curSprintTime < 0){
+				canSprint = false;
+				curSprintTime = 0;
+			}
+		} else {
+			curSprintFactor = 1;
+			curSprintTime += Time.deltaTime;
+			if(curSprintTime > maxSprintTime){
+				canSprint = true;
+				curSprintTime = maxSprintTime;
+			}
+		}
+		/*
+		if (maxSprintTime > 0 && canSprint == true) {
+			curSprintFactor = maxSprintFactor * Input.GetAxis ("Sprint");
+			if (curSprintFactor < 1) {
+				curSprintFactor = 1;
+			}
+			curSprintTime -= Time.deltaTime;
+		} else {
+			curSprintFactor = 1;
+			curSprintTime += Time.deltaTime;
+		}
+
+		if (curSprintTime > maxSprintTime) {
+			curSprintTime = maxSprintTime;
+			canSprint = true;
+		}
+		if (curSprintTime < 0) {
+			curSprintTime = 0;
+			canSprint = false;
+		}
+		*/
+	}
+
+	private void vectorMove(){
+		moveVector.x = maxSpeed * curSprintFactor * Input.GetAxis("HorizontalWalk") * Time.deltaTime;
+		moveVector.y = maxSpeed * curSprintFactor * Input.GetAxis("VerticalWalk") * Time.deltaTime;
+		transform.position += moveVector;
+	}
+}
---
+++ Project Legacy 2D/Assets/Scripts/SpriteDirectionTracking.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class SpriteDirectionTracking : MonoBehaviour {
+
+	public List<Texture2D> upMap, downMap, leftMap, rightMap;
+	private List<Texture2D> vertMap, horMap, finalMap;
+
+	public float distanceX, distanceY;
+	public int activeSprite = 0;
+	public Vector3 lastPosition, curPosition;
+
+	public Animator anim;
+
+	void Start(){
+		anim = GetComponent <Animator>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		FindDirectionTraveled ();
+		DetermineMap ();
+		SendToAnimator ();
+	}
+
+	private void SendToAnimator(){
+		anim.SetFloat ("distanceX", distanceX);
+		anim.SetFloat ("distanceY", distanceY);
+	}
+
+	private void FindDirectionTraveled(){
+		curPosition = transform.position;
+		distanceX = curPosition.x - lastPosition.x;
+		distanceY = curPosition.y - lastPosition.y;
+		lastPosition = transform.position;
+	}
+
+	private void DetermineMap(){
+		if (distanceX > 0) {
+			horMap = rightMap;
+		}
+		if (distanceX < 0) {
+			horMap = leftMap;
+		}
+		if (distanceY > 0) {
+			vertMap = upMap;
+		}
+		if (distanceY < 0) {
+			vertMap = downMap;
+		}
+
+		if (Math.Abs (distanceY) > Math.Abs (distanceX)) {
+			finalMap = vertMap;
+		}
+		if (Math.Abs (distanceY) < Math.Abs (distanceX)) {
+			finalMap = horMap;
+		}
+	}
+}
